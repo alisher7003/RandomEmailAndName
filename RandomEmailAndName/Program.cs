dfbf84249e2e389a7d63baa4eb1403268512985d
alisher7003 <alisher.ishniyozov@gmail.com>
2024-07-03 21:33:36 +0500

    service class methods changed to private

--- RandomEmailAndName/Program.cs
+++ RandomEmailAndName/Program.cs
@@ -4,33 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int selection;
-            string inputString;
-            do
-            {
-                Console.Write("Menu \n1->Random Paragraph\n2->Random Country\n3->Random FullName\n4->Random Email address\nSelect: ");
-                selection = int.Parse(Console.ReadLine());
-                Console.Write("how many random repeats you want: ");
-                int setNumber;
-                setNumber = int.Parse(Console.ReadLine());
-                switch (selection)
-                {
-                    case 1:
-                        RandomService.RandomParagraphs(count: setNumber);
-                        break;
-                    case 2:
-                        RandomService.RandomCountries(count: setNumber);
-                        break;
-                    case 3:
-                        RandomService.CreateRandomFullName(count: setNumber);
-                        break;
-                    case 4:
-                        RandomService.GenerateEmailAddress(count: setNumber);
-                        break;
-                }
-                Console.WriteLine("Do you want to continue? press \"yes\" or any button");
-                inputString = Console.ReadLine().ToLower();
-            } while (inputString == "yes");
+            RandomService.GetMenu();
         }
+
+        
     }
 }
--- RandomEmailAndName/RandomService.cs
+++ RandomEmailAndName/RandomService.cs
@@ -9,7 +9,38 @@ namespace RandomEmailAndName
 {
     internal class RandomService
     {
-        public static void RandomCountries(int count)
+        public static void GetMenu()
+        {
+            int selection;
+            string inputString;
+            do
+            {
+                Console.Write("Menu \n1->Random Paragraph\n2->Random Country\n3->Random FullName\n4->Random Email address\nSelect: ");
+                selection = int.Parse(Console.ReadLine());
+                Console.Write("how many random repeats you want: ");
+                int setNumber;
+                setNumber = int.Parse(Console.ReadLine());
+                switch (selection)
+                {
+                    case 1:
+                        RandomParagraphs(count: setNumber);
+                        break;
+                    case 2:
+                        RandomCountries(count: setNumber);
+                        break;
+                    case 3:
+                        CreateRandomFullName(count: setNumber);
+                        break;
+                    case 4:
+                        GenerateEmailAddress(count: setNumber);
+                        break;
+                }
+                Console.WriteLine("Do you want to continue? press \"yes\" or any button");
+                inputString = Console.ReadLine().ToLower();
+            } while (inputString == "yes");
+        }
+
+        private static void RandomCountries(int count)
         {
             CountryName countryName = new CountryName();
             for (int i = 0; i < count; i++)
@@ -18,13 +49,13 @@ namespace RandomEmailAndName
                 Console.WriteLine(country);
             }
         }
-        public static void RandomParagraphs(int count)
+        private static void RandomParagraphs(int count)
         {
             Lipsum lipsum = new Lipsum(LipsumFlavor.ChildHarold, 3, 10, 10, 20, count);
             string randomWords = lipsum.GetValue();
             Console.WriteLine(randomWords);
         }
-        public static void CreateRandomFullName(int count)
+        private static void CreateRandomFullName(int count)
         {
             RealNames realNames = new RealNames();
             for (int i = 0; i < count; i++)
@@ -33,7 +64,7 @@ namespace RandomEmailAndName
                 Console.WriteLine(randomFullName);
             }
         }
-        public static void GenerateEmailAddress(int count)
+        private static void GenerateEmailAddress(int count)
         {
             EmailAddresses emailGenerator = new EmailAddresses();
             for (int i = 0; i < count; i++)
